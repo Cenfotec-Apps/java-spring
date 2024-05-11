76ce12cf5684b8a9f45b3bb24a9747aed37fad38
olmansansoefe <olman.personal@gmail.com>
2024-05-11 09:04:40 -0600

    init project

---
+++ demo/src/main/java/com/project/demo/logic/entity/user/User.java
@@ -0,0 +1,37 @@
+package com.project.demo.logic.entity.user;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String lastname;
+    private String email;
+
+    // Constructors
+    public User() {}
+
+    public User(String name, String email) {
+        this.name = name;
+        this.email = email;
+    }
+
+    // Getters and setters
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+    public String getLastname() {
+        return lastname;
+    }
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
---
+++ demo/src/main/java/com/project/demo/rest/user/userRestController.java
@@ -0,0 +1,4 @@
+package com.project.demo.rest.user;
+
+public class userRestController {
+}
