881c952295b903bb09a7aec357aa478050d84119
olmansansoefe <olman.personal@gmail.com>
2024-06-28 20:01:36 -0600

    update game

--- src/main/java/com/project/demo/rest/game/GameRestController.java
+++ src/main/java/com/project/demo/rest/game/GameRestController.java
@@ -2,6 +2,7 @@ package com.project.demo.rest.game;
 
 import com.project.demo.logic.entity.game.Game;
 import com.project.demo.logic.entity.game.GameRepository;
+import com.project.demo.logic.entity.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,24 @@ public class GameRestController {
     @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN', 'USER')")
     public List<Game> getAllGames(){
         return gameRepository.findAll();
+    }
+
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasAnyRole('ADMIN', 'SUPER_ADMIN')")
+    public Game updateGame(@PathVariable Long id, @RequestBody Game game) {
+        return gameRepository.findById(id)
+                .map(existingGame -> {
+                    existingGame.setName(game.getName());
+                    existingGame.setDescription(game.getDescription());
+                    existingGame.setImgURL(game.getImgURL());
+                    existingGame.setStatus(game.getStatus());
+                    return gameRepository.save(existingGame);
+                })
+                .orElseGet(() -> {
+                    game.setId(id);
+                    return gameRepository.save(game);
+                });
     }
 
     @PostMapping
